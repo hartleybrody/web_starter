8f16f1ab9e59416eada6de11f40a5d65aa906812
Hartley Brody <hartley.brody@gmail.com>
2014-04-03 23:03:04 -0400

    Added another route to the application that renders templates

--- app.py
+++ app.py
@@ -4,8 +4,10 @@
 # in all the other libraries that your code will be using, so that you
 # can call the functions they define, or instantiate the classes they create.
 
-# import the main class from the "Flask" library that we need to create our web application
-from flask import Flask
+# Import the main class from the "Flask" library that we need to create our
+# web application, as well as the `render_template` function for returning
+# HTML files as responses.
+from flask import Flask, render_template
 
 # Create the variable `app` which is an instance of the Flask class that
 # we just imported. Being an "instance of a class" is something pretty
@@ -28,6 +30,16 @@ def homepage():
 	# there's a logged in user making the requests, you could read or write to a
 	# database or run any kind of functions you want.
     return "Hello World"
+
+
+@app.route("/cool-page/")
+def cool_page():
+	# But ou don't want to have to write the HTML for your page inside python
+	# string. That'd get really annoying quickly. Thankfully, most web frameworks
+	# come with a "template engine" that can take basic HTML files and spit it out
+	# in a response. Here, we created a file called 'cool-page.html' inside the
+	# templates folder, which is where Flask looks for template files by default.
+	return render_template("cool-page.html")
 
 
 # Some boilerplate code that just says "if you're running this from the command
