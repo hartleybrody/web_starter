657c849238e16920e28401546583e0cf524a1d71
Hartley Brody <hartley.brody@gmail.com>
2014-04-03 23:17:02 -0400

    added some dynamic content to a page

--- app.py
+++ app.py
@@ -6,8 +6,10 @@
 
 # Import the main class from the "Flask" library that we need to create our
 # web application, as well as the `render_template` function for returning
-# HTML files as responses.
-from flask import Flask, render_template
+# HTML files as responses, as well as the `request` objects which contains
+# information about the request we just recieved, like the URL and cookies
+# and stuff like that.
+from flask import Flask, render_template, request
 
 # Create the variable `app` which is an instance of the Flask class that
 # we just imported. Being an "instance of a class" is something pretty
@@ -40,6 +42,24 @@ def cool_page():
 	# in a response. Here, we created a file called 'cool-page.html' inside the
 	# templates folder, which is where Flask looks for template files by default.
 	return render_template("cool-page.html")
+
+# Using templates is cool and all, but web applications are supposed to by dynamic,
+# not just showing the same content every time you load the page. So let's make a
+# function that returns different output based on some query arguments that the user
+# passes in. Those are the parts of the URL after the "?" sign.
+@app.route("/dynamic-page/")
+def dynamic_page():
+
+	# If there are no query arguments on the URL (ie length of arguments is 0)
+	# then tell the use to try adding some.
+	if len(request.args) == 0:
+		return "Add some query arguments to the end of the URL! Like this ?name=Ben"
+
+	# Here, we're rendering a different template, and passing the query arguments
+	# to the template to be mixed into the content. This will create a variable
+	# called `user_data` that we can access in the template, and it contains the
+	# values that were in the query arguments.
+	return render_template("dynamic-page.html", user_data=request.args)
 
 
 # Some boilerplate code that just says "if you're running this from the command
