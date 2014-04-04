501ef56581169299c805e55b90334dc981747828
Hartley Brody <hartley.brody@gmail.com>
2014-04-04 01:10:25 -0400

    convert from tab indentation to spaces

--- app.py
+++ app.py
@@ -7,8 +7,8 @@
 # Import the main class from the "Flask" library that we need to create our
 # web application, as well as the `render_template` function for returning
 # HTML files as responses, as well as the `request` objects which contains
-# information about the request we just recieved, like the URL and cookies
-# and stuff like that.
+# information about requests that we recieve, like the URL and cookies and
+# stuff like that.
 from flask import Flask, render_template, request
 
 # Create the variable `app` which is an instance of the Flask class that
@@ -27,39 +27,39 @@ app = Flask(__name__)
 @app.route("/")
 def homepage():
     
-	# Inside this function, you can run whatever logic you want, running any python
-	# code your heart desires. You could check the cookies on the request to see if
-	# there's a logged in user making the requests, you could read or write to a
-	# database or run any kind of functions you want.
+    # Inside this function, you can run whatever logic you want, running any python
+    # code your heart desires. You could check the cookies on the request to see if
+    # there's a logged in user making the requests, you could read or write to a
+    # database or run any kind of functions you want.
     return "Hello World"
 
 
 @app.route("/cool-page/")
 def cool_page():
-	# But ou don't want to have to write the HTML for your page inside python
-	# string. That'd get really annoying quickly. Thankfully, most web frameworks
-	# come with a "template engine" that can take basic HTML files and spit it out
-	# in a response. Here, we created a file called 'cool-page.html' inside the
-	# templates folder, which is where Flask looks for template files by default.
-	return render_template("cool-page.html")
+    # But you don't want to have to write the HTML for your pages inside a python
+    # string. That'd get really annoying quickly. Thankfully, most web frameworks
+    # come with a "template engine" that can take basic HTML files and spit it out
+    # in a response. Here, we created a file called 'cool-page.html' inside the
+    # templates folder, which is where Flask looks for template files by default.
+    return render_template("cool-page.html")
 
 # Using templates is cool and all, but web applications are supposed to by dynamic,
-# not just showing the same content every time you load the page. So let's make a
-# function that returns different output based on some query arguments that the user
+# not just showing the same content every time you load the page. So let's build a
+# page that returns different output based on some query arguments that the user
 # passes in. Those are the parts of the URL after the "?" sign.
 @app.route("/dynamic-page/")
 def dynamic_page():
 
-	# If there are no query arguments on the URL (ie length of arguments is 0)
-	# then tell the use to try adding some.
-	if len(request.args) == 0:
-		return "Add some query arguments to the end of the URL! Like this ?name=Ben"
+    # If there are no query arguments on the URL (ie length of query arguments is 0)
+    # then tell the user to try adding some.
+    if len(request.args) == 0:
+        return "Add some query arguments to the end of the URL! Like this ?name=Ben"
 
-	# Here, we're rendering a different template, and passing the query arguments
-	# to the template to be mixed into the content. This will create a variable
-	# called `user_data` that we can access in the template, and it contains the
-	# values that were in the query arguments.
-	return render_template("dynamic-page.html", user_data=request.args)
+    # Here, we're rendering a different template, and passing the query arguments
+    # to the template to be mixed into the content. This will create a variable
+    # called `user_data` that we can access in the HTML template, and it contains the
+    # values that were in the query arguments.
+    return render_template("dynamic-page.html", user_data=request.args)
 
 
 # Some boilerplate code that just says "if you're running this from the command
